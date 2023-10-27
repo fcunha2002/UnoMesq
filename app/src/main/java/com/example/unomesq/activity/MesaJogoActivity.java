3670fdd1e20a2d211f041ba705d074cbea4cee02
fabiom <fabiom@mesquita.com.br>
2023-10-27 20:19:40 -0300

    Mais cartas e aumento do baralho.

--- app/src/main/java/com/example/unomesq/activity/MesaJogoActivity.java
+++ app/src/main/java/com/example/unomesq/activity/MesaJogoActivity.java
@@ -11,6 +11,6 @@ public class MesaJogoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interface_teste);
+        setContentView(R.layout.activity_mesa_jogo);
     }
 }

fdbe3db7def10a9c2886914614c4a61ec87802b1
thefato <bielfc06@gmail.com>
2023-10-27 19:34:05 -0300

    Primeira versão tela de jogo: Larissa, Darcio, Victor, Lucas

---
+++ app/src/main/java/com/example/unomesq/InterfaceTeste.java
@@ -0,0 +1,14 @@
+package com.example.unomesq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterfaceTeste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface_teste);
+    }
+}
